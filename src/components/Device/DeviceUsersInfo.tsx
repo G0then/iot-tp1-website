c0b8960aee7551a684806c25e6876509d8603799
G0then <nelsonsilva19999@gmail.com>
2023-04-04 18:17:20 +0100

    Add view button to devices user

--- src/components/Device/DeviceUsersInfo.tsx
+++ src/components/Device/DeviceUsersInfo.tsx
@@ -3,44 +3,66 @@
 import { UserDto } from "@/types/user";
 import { useQuery } from "@/utils/requests/getSwr";
 import { GridColDef } from "@mui/x-data-grid";
-import { DateTime } from "luxon";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useMemo } from "react";
 import { NoData } from "../Error/NoData";
 import { LoadingData } from "../Loading/LoadingData";
 import Table from "../Table/Table";
-
-const columns: GridColDef[] = [
-  {
-    field: "username",
-    headerName: "Username",
-    flex: 1,
-    headerAlign: "left",
-  },
-  {
-    field: "name",
-    headerName: "Name",
-    flex: 1,
-    headerAlign: "left",
-  },
-  {
-    field: "email",
-    headerName: "Email",
-    flex: 1,
-    headerAlign: "left",
-  },
-];
+import ViewButton from "../Button/ViewButton";
 
 type DeviceUsersInfoProps = {
   device_pid: string;
 };
 
 export default function DeviceUsersInfo({ device_pid }: DeviceUsersInfoProps) {
+  const { push } = useRouter();
   const urlGetDeviceUsers = `devices/${device_pid}/users`;
   const {
     data: deviceUsers,
     isLoading: deviceUsersLoading,
     error: deviceUsersError,
   } = useQuery<UserDto[]>(urlGetDeviceUsers);
+
+  const columns: GridColDef[] = useMemo(() =>  [
+    {
+      field: "username",
+      headerName: "Username",
+      flex: 1,
+      headerAlign: "left",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      headerAlign: "left",
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+      headerAlign: "left",
+    },
+    {
+      field: "view",
+      headerName: "View",
+      // minWidth: 350,
+      flex: 1,
+      // filterable: false,
+      // resizable: true,
+      headerAlign: "left",
+      // align: 'center',
+      renderCell: (params) => {
+        const onClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+          e.stopPropagation(); // don't select this row after clicking
+  
+          push(`/users/${params.row.username}`);
+        };
+  
+        return <ViewButton onClick={onClick} />;
+      },
+    },
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  ],[]);
 
   //Ocorreu um erro
   if (deviceUsersError) {
